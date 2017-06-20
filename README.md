510db8c600b191af705262fc83019c7de0b66902
Tai-Yi Kuo <b96705008@gmail.com>
2017-06-20 19:17:24 +0800

    cache on redis

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 ## Functions
 * PyMongo
 * CORS
+* Cache
 * Page Query
 
 ## TODO
@@ -14,22 +15,34 @@
 
 ## Example
 ```
-http://127.0.0.1:5000/actors/1/events?page=1
+http://127.0.0.1:5000/actors/1/events?page_size=2&page=1
 ```
 
+### response
 ```
 {
   "next_page": {
     "has_next": true, 
-    "next_page_url": "?page_size=1&page=2", 
+    "next_page_url": "?page_size=2&page=2", 
     "page": 2, 
-    "page_size": 1
+    "page_size": 2
   }, 
   "result": [
     {
       "_id": "594885e4994aa607c71983b8", 
       "action": {
         "time": 5.0, 
+        "type": "love"
+      }, 
+      "actor": {
+        "id": "1", 
+        "type": "customer_id"
+      }
+    }, 
+    {
+      "_id": "594885d1994aa607c71983b7", 
+      "action": {
+        "time": 4.0, 
         "type": "love"
       }, 
       "actor": {
--- app/mongo.py
+++ app/mongo.py
@@ -4,11 +4,13 @@ import sys
 import configparser
 
 from flask import Flask
+from flask import Blueprint
 from flask import jsonify
 from flask import request
 
 from flask_pymongo import PyMongo
 from flask_cors import CORS, cross_origin
+from flask_cache import Cache
 
 from page_query import PageQuery
 
@@ -16,6 +18,7 @@ from page_query import PageQuery
 conf_path = sys.argv[1]
 config = configparser.ConfigParser()
 config.read(conf_path)
+HIPPO_NAME = config.get('hippo', 'name')
 EVENT_COLLECION = config.get('mongo', 'collection')
 DEFAULT_PAGE_SIZE = config.get('api', 'page_size')
 
@@ -25,30 +28,46 @@ app = Flask(__name__)
 # cross domain
 CORS(app)
 
+# cache
+cache = Cache(app, config={
+    'CACHE_TYPE': 'redis',
+    'CACHE_REDIS_HOST': config.get('redis', 'host'),
+    'CACHE_REDIS_PORT': config.getint('redis', 'port')
+})
+
 # connect MongoDB
 app.config['MONGO_DBNAME'] = config.get('mongo', 'db')
 app.config['MONGO_URI'] = config.get('mongo', 'uri')
 mongo = PyMongo(app)
 
+# api service
+service = Blueprint(HIPPO_NAME, __name__)
 
-@app.route('/actors/<actor_id>/events', methods=['GET'])
+def make_key():
+  """Make a key that includes GET parameters."""
+  return request.full_path
+
+@service.route('/actors/<actor_id>/events', methods=['GET'])
+@cache.cached(key_prefix=make_key, timeout=10)
 def get_all_events(actor_id):
     event = mongo.db[EVENT_COLLECION]
     pagesize = int(request.args.get('page_size', default=DEFAULT_PAGE_SIZE))
     page = int(request.args.get('page', default=1))
-
     query = event \
         .find({'actor.id': actor_id}) \
         .sort('action.time', -1)
     
     page_query = PageQuery(pagesize, page, query)
     output = page_query.get_output()
+    print('query db...')
     return jsonify(output)
 
 
 if __name__ == '__main__':
     HOST = config.get('client', 'host')
     PORT = config.getint('client', 'port')
+    URL_PREFIX = config.get('api', 'url_prefix')
+    app.register_blueprint(service, url_prefix='/' + URL_PREFIX)
     app.run(debug=True, host=HOST, port=PORT)
 
 
