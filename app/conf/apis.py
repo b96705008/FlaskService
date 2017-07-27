35c747b88cfd39ed344ad57d41e80d5a6fa5984a
Tai-Yi Kuo <b96705008@gmail.com>
2017-07-27 17:17:44 +0800

    api loader refactor

--- app/conf/apis.py
+++ app/conf/apis.py
@@ -3,6 +3,11 @@ from api import *
 
 def load_apis(config, tools, models):
     apis = []
-    for get_api in [get_todo_api, get_journey_api]:
+    mod = __import__('api')
+    api_attrs = filter(lambda attr: attr[:3] == 'get' and attr[-3:] == 'api',
+                       dir(mod))
+    get_api_funcs = map(lambda attr: getattr(mod, attr), api_attrs)
+
+    for get_api in get_api_funcs:
         apis.append(get_api(config, tools, models))
     return apis
